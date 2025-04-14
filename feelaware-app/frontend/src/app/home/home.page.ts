994726d45a06f2ba4ef038eee39b07d5102fb9e8
nathanbuyr <G00421082@atu.ie>
2025-04-14 23:22:26 +0100

    Filled home page with simple redirecting buttons to other pages

--- feelaware-app/frontend/src/app/home/home.page.ts
+++ feelaware-app/frontend/src/app/home/home.page.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonButton } from '@ionic/angular/standalone';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
+  standalone: true,
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonButton, RouterLink],
 })
 export class HomePage {
   constructor() {}
